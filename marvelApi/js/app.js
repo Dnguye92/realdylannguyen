1bfd240e50f73d10c02420f7c18d77ae24bc471d
Dylan Nguyen <Dnguye92@yahoo.com>
2017-06-02 08:52:50 -0700

    adding media queries to farm:table

--- marvelApi/js/app.js
+++ marvelApi/js/app.js
@@ -1,6 +1,6 @@
 var app = angular.module('Marvel', ['ui.router', 'infinite-scroll'])
 
-app.config(function($stateProvider) {
+app.config(function($stateProvider, $locationProvider) {
 	$stateProvider.state('index', {
 		url: '',
 		controller: 'mainCtrl',
@@ -9,8 +9,9 @@ app.config(function($stateProvider) {
 	.state('index.single', {
 		url: '/:id',
 		controller: 'singleCharacter',
-		templateUrl: 'marvelApi/templates/characterPopup.html'
+		templateUrl: 'marvelApi/templates/characterPopUp.html'
 	})
+	$locationProvider.html5Mode(true)
 })
 
 app.controller('mainCtrl', function($scope, ComicBooks) {
@@ -54,12 +55,13 @@ app.directive('popup', function() {
 app.factory('ComicBooks', function($http, $q) {
 	// For client side
 	var publicKey = '2ac5db3fa3e7c593eeb5ee35d7ca625d'
+	var hash = '1abc2af7ed20b032b4bd3fb762fc89f15693f0f3'
 	var baseUrl = 'http://gateway.marvel.com/v1/'
 	var limit = 50
 
 	var find = function() {
 		var def = $q.defer()
-		var url = baseUrl + 'public/characters/' + id + '?apikey=' + publicKey
+		var url = baseUrl + 'public/characters/' + id + 'ts=1&apikey=' + publicKey + '&hash=' + hash
 		$http.get(url).success(def.resolve).error(def.reject)
 
 		return def.promise
@@ -67,7 +69,7 @@ app.factory('ComicBooks', function($http, $q) {
 
 	var findOne = function(id) {
 		var def = $q.defer()
-		var url = baseUrl + 'public/characters/' + id + '?apikey=' + publicKey
+		var url = baseUrl + 'public/characters/' + id + '?apikey=' + publicKey + '&hash=' + hash
 		$http.get(url).success(def.resolve).error(def.reject)
 
 		return def.promise
@@ -75,7 +77,7 @@ app.factory('ComicBooks', function($http, $q) {
 
 	var findNext = function(offset) {
 		var def = $q.defer()
-		var url = baseUrl + 'public/characters?limit=' + limit + '&offset=' + (limit * offset) + '&apikey=' + publicKey
+		var url = baseUrl + 'public/characters?limit=' + limit + '&offset=' + (limit * offset) + '&apikey=' + publicKey + '&hash=' + hash
 		$http.get(url).success(def.resolve).error(def.reject)
 
 		return def.promise
